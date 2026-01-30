d0f7927016b080b132cfe357f3bb1eb15a208be1
Rizkyaditiya21 <rizkyaditiyafernando@gmail.com>
2026-01-30 16:55:52 +0700

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -47,7 +47,34 @@ function checkoutWA() {
   });
 
   text += "%0ATotal: Rp " + total.toLocaleString();
-  window.open("https://wa.me/6281234567890?text=" + text);
+  window.<a class="checkout" onclick="kePayment()">Bayar Sekarang</a>
+
 }
 
 loadCart();
+function kePayment(){
+  window.location.href = "payment.html";
+}
+
+function konfirmasiBayar(){
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let text = "Halo FITLAS, saya sudah bayar.%0A%0APesanan:%0A";
+  let total = 0;
+
+  cart.forEach(item=>{
+    text += "- " + item.name + "%0A";
+    total += item.price;
+  });
+
+  text += "%0ATotal: Rp " + total.toLocaleString();
+  window.<a class="checkout" onclick="kePayment()">Bayar Sekarang</a>
+
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+  let el = document.getElementById("totalBayar");
+  if(!el) return;
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let total = cart.reduce((s,i)=>s+i.price,0);
+  el.innerText = "Total: Rp " + total.toLocaleString();
+});
