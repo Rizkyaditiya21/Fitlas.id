c6f8dcdcf9f7179eb5cbc51310314585769551af
Rizkyaditiya21 <rizkyaditiyafernando@gmail.com>
2026-01-31 12:19:33 +0700

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,14 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 function addToCart(name, price, image) {
-  cart.push({ name, price, image });
+  cart.push({name, price, image});
   localStorage.setItem("cart", JSON.stringify(cart));
   alert("Produk masuk ke keranjang");
 }
 
 function loadCart() {
   let container = document.getElementById("cartItems");
-  if (!container) return;
-
+  if(!container) return;
   container.innerHTML = "";
   let total = 0;
 
@@ -27,27 +26,17 @@ function loadCart() {
     `;
   });
 
-  document.getElementById("total").innerText =
-    "Total: Rp " + total.toLocaleString();
+  document.getElementById("total").innerText = "Total: Rp " + total.toLocaleString();
 }
 
-function removeItem(index) {
-  cart.splice(index, 1);
+function removeItem(index){
+  cart.splice(index,1);
   localStorage.setItem("cart", JSON.stringify(cart));
   loadCart();
 }
 
-function checkoutWA() {
-  let text = "Halo FITLAS, saya mau pesan:%0A";
-  let total = 0;
-
-  cart.forEach(item => {
-    text += "- " + item.name + "%0A";
-    total += item.price;
-  });
-
-  text += "%0ATotal: Rp " + total.toLocaleString();
-  window.open("https://wa.me/6281234567890?text=" + text);
+function kePayment() {
+  window.location.href = "payment.html";
 }
 
 loadCart();
